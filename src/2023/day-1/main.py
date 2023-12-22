d33797bfae23944af1710eb8185b8a7ad022259f
Vishesh Gupta <vgupta@clearstreet.io>
2023-12-21 21:39:46 -0500

    Optimized the code for day1 part II

--- src/2023/day-1/main.py
+++ src/2023/day-1/main.py
@@ -1,47 +1,65 @@
-## Day 1: Trebuchet? 
+## Day 1: Trebuchet?
+import argparse
+
+words_array = {
+    "one": '1',
+    "two": '2',
+    "three": '3',
+    "four": '4',
+    "five": '5',
+    "six": '6',
+    "seven": '7',
+    "eight": '8',
+    "nine": '9'
+}
+
+def convert_words_to_num_gen(line: str, part_two: bool = False):
+    first_digit = -1
+    last_digit = -1
+    for i in range(0, len(line)):
+        if line[i].isdigit():
+            if first_digit == -1:
+                first_digit = int(line[i])
+                continue
+            last_digit = int(line[i])
+            continue
+        if part_two == True:
+            for j in range(0, len(line)+1):
+                if line[i:j] in words_array.keys():
+                    if first_digit == -1:
+                        first_digit = int(words_array[line[i:j]])
+                        i += j
+                        break
+                    else:
+                        last_digit = int(words_array[line[i:j]])
+    if last_digit == -1:
+        last_digit = first_digit
+
+    return first_digit, last_digit
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+
+    # Adding positional arguments
+    parser.add_argument('input_file', help='Path to the input file')
+    parser.add_argument('--enable_part_two', help="enable or disable for part two", required=False)
+
+    # Parsing the arguments
+    args = parser.parse_args()
+
+    # Accessing the values
+    input_file = args.input_file
+    enable_part_two = True if args.enable_part_two == 'True' else False
+
+    with open(input_file) as file:
+        lines = file.readlines()
+
+    sum = 0
+    for l in lines:
+        f,l = convert_words_to_num_gen(l, enable_part_two)
+        sum += ((f * 10) + l)
+    print(sum)
 
 
-def convert_words_to_num(line: str):
-    words_array = {
-        "one": '1',
-        "two": '2',
-        "three": '3',
-        "four": '4',
-        "five": '5',
-        "six": '6',
-        "seven": '7',
-        "eight": '8',
-        "nine": '9'
-    }
-
-    for i in  words_array.keys():
-        line = line.replace(i, words_array[i])
-
-    return line        
-
-def find_first_number(line: str): 
-    for i in line:
-        if i >= '0'  and i <= '9':
-            return int(i)
-
-
-with open("sample2") as input_file:
-    lines = input_file.readlines()
-
-print(lines)
-
-sum = 0
-new_count = 0
-for l in lines:
-    # first_number, last_number = [find_first_number(l), find_first_number(reversed(l))]
-    # count = first_number * 10 + last_number
-    # sum += count
-    # count = 0
-    new_line = convert_words_to_num(l)
-    print(new_line)
-    first_number, last_number = [find_first_number(new_line), find_first_number(reversed(new_line))]
-    print(first_number, last_number)
-    count = first_number * 10 + last_number
-    new_count += count
-
-print(sum)
+if __name__ == '__main__':
+    main()
